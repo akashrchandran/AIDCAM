219f1fb74f48e30b49deddc3877fda408dc13cdb
Akash R Chandran <chandranrakash@gmail.com>
2023-04-09 10:51:25 +0530

    updated

--- Model training/camera.py
+++ Model training/camera.py
@@ -7,22 +7,22 @@ model = AccidentDetectionModel("model.json", 'model_weights.h5')
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 def startapplication():
-    video = cv2.VideoCapture('cars.mp4') # for camera use video = cv2.VideoCapture(0)
+    video = cv2.VideoCapture('ksrtc.mp4') # for camera use video = cv2.VideoCapture(0)
     while True:
         ret, frame = video.read()
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         roi = cv2.resize(gray_frame, (250, 250))
 
         pred, prob = model.predict_accident(roi[np.newaxis, :, :])
-        if(pred == "Accident"):
+        if (pred == "Accident"):
             prob = (round(prob[0][0]*100, 2))
-            
+
             # to beep when alert:
             # if(prob > 90):
             #     os.system("say beep")
 
             cv2.rectangle(frame, (0, 0), (280, 40), (0, 0, 0), -1)
-            cv2.putText(frame, pred+" "+str(prob), (20, 30), font, 1, (255, 255, 0), 2)
+            cv2.putText(frame, f"{pred} {str(prob)}", (20, 30), font, 1, (255, 255, 0), 2)
 
         if cv2.waitKey(33) & 0xFF == ord('q'):
             return
